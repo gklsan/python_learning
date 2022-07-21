1a135137bbbc642fa47728c950f31589f505064b
Gokul <pgokulmca@gmail.com>
2022-07-21 13:07:41 +0530

    variable type checks with array

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import random
 
 # printing hello there
 print("hello there!")
@@ -38,3 +39,58 @@ MYVAR = 'MYVAR'
 MYVAR = 'MYVAR'
 my_var1 = 'my_var1'
 print(myvar, my_var, _myvar, myVar, MY_VAR, MYVAR, my_var1)
+
+# multiple values assignment
+a, b, c = 1, 2, 3
+print(a, b, c)
+
+a, b, c = [4, 5, 6]
+print(a, b, c)
+
+a = b = c = 'values'
+print(a, b, c)
+
+a = 1
+
+print(b + c, a)
+
+
+
+def myFun():
+    global x
+    x = 'Global variable'
+    y = 'Local variable'
+    print('This is myFun', a)
+    print('This is ' + x)
+
+myFun()
+print(x)
+
+def printType(input):
+    print(type(input), input)
+
+printType(1j)
+printType([])
+printType(range(5))
+printType({})
+printType({a: 1, 'b': 2, 3: 4, })
+printType(dict({'name': 1, 'b': 2, 3: 4, }))
+printType({a, b})
+printType({a, b, 4})
+printType(frozenset({a: 1}))
+printType(frozenset({a, b, 4}))
+printType(True)
+printType(False)
+
+# print random number
+printType(random.randrange(10))
+printType(random.randrange(7, 10))
+
+
+# Integer array
+input = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+print(input[2])
+print(input[2:6])
+print(input[12:])
+print(input[:7])
+print(input[:-5])
